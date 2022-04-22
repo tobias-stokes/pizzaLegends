a917bb97f5de42034bb0f6dd4a709a1fcc4b062f
Tobias Stokes <tobias.stokes@meshbar.com.au>
2022-04-22 15:17:47 +1000

    Sprite and GameObject classes

--- public/scripts/overworld.js
+++ public/scripts/overworld.js
@@ -12,39 +12,20 @@ class Overworld {
             };
             image.src = '/images/maps/DemoLower.png';
 
-        const x = 5;
-        const y = 6;
-
-        const shadow = new Image();
-            shadow.onload = () => {
-                this.ctx.drawImage(
-                    shadow,
-                    0,  // Left cut
-                    0,  // Top cut
-                    32, // Width
-                    32, // Height
-                    x * 16 - 8,
-                    y * 16 - 18,
-                    32,
-                    32
-                    );
-            };
-            shadow.src = '/images/characters/shadow.png';
-
-        const hero = new Image();
-            hero.onload = () => {
-                this.ctx.drawImage(
-                    hero,
-                    0,  // Left cut
-                    0,  // Top cut
-                    32, // Width
-                    32, // Height
-                    x * 16 - 8,
-                    y * 16 - 18,
-                    32,
-                    32
-                    );
-            };
-            hero.src = '/images/characters/people/hero.png';
+        // Place some game objects
+            const hero = new GameObject({
+                x: 5,
+                y: 6
+            });
+            const npc1 = new GameObject({
+                x: 7,
+                y: 9,
+                src: '/images/characters/people/npc1.png'
+            });
+            
+            setTimeout(() => {
+                hero.sprite.draw(this.ctx);
+                npc1.sprite.draw(this.ctx);
+            }, 200);
     }
 }
