cc9bc1146e05901aed2fb83636d588927b927f62
Tobias Stokes <tobias.stokes@meshbar.com.au>
2022-04-23 12:16:16 +1000

    walls and collisions

--- public/scripts/overworld.js
+++ public/scripts/overworld.js
@@ -17,7 +17,8 @@ class Overworld {
         // Update all updates
             Object.values(this.map.gameObjects).forEach(object => {
                 object.update({
-                    arrow: this.directionInput.direction
+                    arrow: this.directionInput.direction,
+                    map: this.map
                 });
             });
 
@@ -41,6 +42,7 @@ class Overworld {
 
     init() {
         this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+        this.map.mountObjects();
 
         this.directionInput = new DirectionInput();
         this.directionInput.init();
