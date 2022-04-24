c75f3e1bfe8d818ce0892790b4f4e7f5bc370cb8
Tobias Stokes <tobias.stokes@meshbar.com.au>
2022-04-24 12:21:38 +1000

    init.js

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -1,7 +1,5 @@
-(function() {
-    const overworld = new Overworld({
-        element: document.querySelector('.game-container')
-    });
+const overworld = new Overworld({
+    element: document.querySelector('.game-container')
+});
 
-    overworld.init();
-})();
+overworld.init();
